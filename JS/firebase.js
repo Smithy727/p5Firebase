5b0ba985558c3267ea15259e4ce1395b038691d8
Smithy727 <86169484+Smithy727@users.noreply.github.com>
2023-02-16 12:58:19 +0000

    Add files via upload

---
+++ JS/firebase.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyCZnKyW3dohqBtRig9O7aHniRVmhBNxtuU",
+  authDomain: "fir-p5-69cc1.firebaseapp.com",
+  projectId: "fir-p5-69cc1",
+  storageBucket: "fir-p5-69cc1.appspot.com",
+  messagingSenderId: "367308444170",
+  appId: "1:367308444170:web:098848b3062340b3cfc5f3",
+  measurementId: "G-1X8DCD8J77"
+});
+
+var db = firebase.firestore()
---
+++ JS/script.js
@@ -0,0 +1,16 @@
+var circleForm = document.querySelector("#circleForm");
+
+circleForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    console.log(circleForm.color.value);
+    db.collection("circles").doc().set({
+        color: circleForm.color.value, 
+        size: circleForm.size.value,
+        x: circleForm.x.value,
+        y: circleForm.y.value,
+
+    }).then(()=>{
+        location.reload();
+    })
+
+})
